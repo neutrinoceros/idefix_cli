2066a8fb16a816dcc261896776f3fa731a64aa57
Clément Robert <cr52@protonmail.com>
2021-03-13 11:42:04 +0100

    add test for idfx read

---
+++ tests/test_read.py
@@ -0,0 +1,23 @@
+import json
+
+from idefix_cli.main import main
+
+
+def test_read_not_a_file(tmp_path, capsys):
+    target = tmp_path / "not_a_file"
+    ret = main(["read", str(target.absolute())])
+    assert ret != 0
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == f"Error: no such file `{target.absolute()}`.\n"
+
+
+def test_read(inifile, capsys):
+    ret = main(["read", str(inifile.absolute())])
+    assert ret == 0
+
+    out, err = capsys.readouterr()
+
+    # json validation
+    json.loads(out)
+    assert err == ""
