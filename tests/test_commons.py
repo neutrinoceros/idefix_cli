a303c11fa6deea6b0bae1e4c1427cfd27aecd537
Clément Robert <cr52@protonmail.com>
2021-03-24 12:03:22 +0100

    adapt tests to the new requires_idefix decorator

--- tests/test_commons.py
+++ tests/test_commons.py
@@ -1,21 +1,20 @@
 import sys
 
-import pytest
-
 from idefix_cli._commons import requires_idefix
 
 
 @requires_idefix()
-def noop():
+def noop() -> int:
+    """A fake command"""
     print("hello stdout")
     print("hello stderr", file=sys.stderr)
-    return "success"
+    return 0
 
 
 def test_requires_idefix_undef(capsys, monkeypatch):
     monkeypatch.delenv("IDEFIX_DIR", raising=False)
-    with pytest.raises(SystemExit):
-        noop()
+    ret = noop()
+    assert ret == 10
 
     out, err = capsys.readouterr()
     assert out == ""
@@ -26,7 +25,7 @@ def test_requires_idefix_def(capsys, monkeypatch, tmp_path):
     monkeypatch.setenv("IDEFIX_DIR", str(tmp_path / "idefix"))
 
     ret = noop()
-    assert ret == "success"
+    assert ret == 0
 
     out, err = capsys.readouterr()
     assert out == "hello stdout\n"
