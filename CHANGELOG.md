f90fc3b72248a08b4b11891d2486c8a116cebd73
Clément Robert <cr52@protonmail.com>
2021-08-17 19:59:33 +0200

    UX: add colored logging and refactor idfx clone to use os.replace for atomicity

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.2] - 2021-17-08
+
+UX: add colored logging and refactor idfx clone to use os.replace for atomicity
+
 ## [0.4.1] - 2021-08-05
 
 UX: add a more specific error message when IDEFIX_DIR exists but isn't valid.
--- idefix_cli/commands/_clone.py
+++ idefix_cli/commands/_clone.py
@@ -1,11 +1,12 @@
 import os
 import shutil
-import sys
 from glob import glob
 from tempfile import TemporaryDirectory
 from typing import Optional
 
-from idefix_cli._commons import files_from_patterns
+from rich import print
+
+from idefix_cli._commons import files_from_patterns, print_err
 
 minimal_target = frozenset(
     (
@@ -43,33 +44,45 @@ def clone(
     extra: Optional[list[str]] = None,
 ) -> int:
     if not os.path.isdir(source):
-        print(f"Error: source directory not found {source}", file=sys.stderr)
+        print_err(f"Error: source directory not found {source}")
+        return 1
+    if not os.listdir(source):
+        print_err(f"Error: {source} appears to be empty.")
         return 1
     if os.path.exists(dest):
-        print(f"Error: destination directory exists {dest}", file=sys.stderr)
+        print_err(f"Error: destination directory exists {dest}")
         return 1
 
     if extra is None:
         extra = []
 
-    with TemporaryDirectory() as tmpdir:
+    files_to_generate = files_from_patterns(source, *minimal_target, *extra)
+    if not files_to_generate:
+        print_err(f"Error: did not find any file to copy from {source}")
+        return 1
+
+    with TemporaryDirectory(dir=os.path.dirname(dest)) as tmpdir:
         # using a context manager to guarantee atomicity:
         # either it's a full success or we don't copy anything
+        # The temporary directory is created next to the final destination
+        # so we can safely use os.replace (atomic) without worrying about
+        # possibly sparse filesystems.
         for file in files_from_patterns(source, *minimal_target, *extra):
             fdest = os.path.join(tmpdir, os.path.basename(file))
             if shallow:
                 os.symlink(file, fdest)
             else:
                 shutil.copy(file, fdest)
-        # using shutil.move over os.replace because the former works with
-        # cross-devices links while the latter doesn't, which is important in
-        # large facilities.
-        shutil.move(tmpdir, dest)
+        os.replace(tmpdir, dest)
 
+    output_files = list(glob(os.path.join(dest, "*")))
     if shallow:
-        objs = f"symlinks to {source}"
+        objs = "symlinks"
+        lencol1 = max(len(_) for _ in output_files)
+        lines = [f"{_.ljust(lencol1 + 1)} -> {os.readlink(_)}" for _ in output_files]
     else:
         objs = "files"
+        lines = output_files
     output_files = "\n".join(list(glob(os.path.join(dest, "*"))))
-    print(f"Created the following {objs}\n{output_files}")
+    print(f"Created the following {objs}\n" + "\n".join(lines))
     return 0
