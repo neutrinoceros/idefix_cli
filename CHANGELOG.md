c4c5ca226a56caff39be4063725d98118b3afa90
Clément Robert <cr52@protonmail.com>
2021-12-25 12:16:26 +0100

    ENH: improve command plugin validation and increase main module test coverage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.11.1] - 2021-12-25
+ENH: improve command plugin validation and increase main module test coverage
+https://github.com/neutrinoceros/idefix_cli/pull/95
+
+
 ## [0.11.0] - 2021-12-24
 
 - ENH: improvements to `idfx clean`
---
+++ tests/test_app_structure.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import re
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import pytest
+
+from idefix_cli._main import _setup_commands
+from idefix_cli._main import main
+
+SRC_DIR = Path(__file__).parents[1].joinpath("idefix_cli")
+
+
+@pytest.mark.parametrize(
+    "content, msg",
+    (
+        (("\n"), " is missing required functions " "'command' and 'add_arguments'"),
+        (
+            ("def add_arguments():\n", "    return\n"),
+            " is missing a 'command' function",
+        ),
+        (
+            ("def command():\n", "    return\n"),
+            " is missing a 'add_arguments' function",
+        ),
+        (
+            (
+                "def add_arguments():\n",
+                "    return\n",
+                "def command():\n",
+                "    return\n",
+            ),
+            ".add_arguments function's signature is invalid. "
+            "Expected a single argument named 'parser', found []",
+        ),
+    ),
+)
+def test_broken_command_plugin(content, msg):
+    with NamedTemporaryFile(
+        mode="w", dir=str(SRC_DIR / "_commands"), suffix=".py"
+    ) as fh:
+        fh.writelines(content)
+        fh.seek(0)  # force flush
+        module_name, _ = os.path.splitext(os.path.basename(fh.name))
+        with pytest.raises(
+            RuntimeError,
+            match=re.escape(f"command plugin {module_name}{msg}"),
+        ):
+            _setup_commands(argparse.ArgumentParser())
+
+
+def test_unknown_args(capsys):
+    args = "--an_invalid_argument", "-another"
+    ret = main(["clean", *args])
+    assert ret != 0
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == f"ERROR received unknown arguments {args!r}.\n"
