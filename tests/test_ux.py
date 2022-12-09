32552ba8c5a0ae38352d1cc7f3eac6d3537f5491
Clément Robert <cr52@protonmail.com>
2022-12-09 23:46:11 +0100

    UX: rework help

--- tests/test_ux.py
+++ tests/test_ux.py
@@ -1,5 +1,6 @@
 import sys
 
+import pytest
 from pytest import assume
 
 from idefix_cli._main import main
@@ -11,7 +12,11 @@ else:
 HELP_MESSAGE = (
     "usage: idfx [-h] [-v] {clean,clone,conf,read,run,stamp,write} ...\n"
     "\n"
-    "positional arguments:\n"
+    f"{OPTIONAL_SEC}:\n"
+    "  -h, --help            show this help message and exit\n"
+    "  -v, --version         show program's version number and exit\n"
+    "\n"
+    "commands:\n"
     "  {clean,clone,conf,read,run,stamp,write}\n"
     "    clean               remove compilation files\n"
     "    clone               clone a problem directory\n"
@@ -20,13 +25,10 @@ HELP_MESSAGE = (
     "    run                 run an Idefix problem\n"
     "    stamp               print relevant data for reproduction to stdout\n"
     "    write               write an Idefix inifile from a json string\n"
-    "\n"
-    f"{OPTIONAL_SEC}:\n"
-    "  -h, --help            show this help message and exit\n"
-    "  -v, --version         show program's version number and exit\n"
 )
 
 
+@pytest.mark.usefixtures("isolated_conf_dir")
 def test_no_command_passed(capsys):
     ret = main([])
     assume(ret != 0)
