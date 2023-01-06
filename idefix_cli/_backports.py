5f19f4346d0249d4a001b6adf11ef738167836ff
Clément Robert <cr52@protonmail.com>
2023-01-06 11:29:16 +0100

    ENH: move backports to a separate module

---
+++ idefix_cli/_backports.py
@@ -0,0 +1,66 @@
+import os
+import sys
+
+if sys.version_info >= (3, 11):
+    pass
+else:
+    # vendored from Python 3.11b1
+    from contextlib import AbstractContextManager
+
+    class chdir(AbstractContextManager):
+        """Non thread-safe context manager to change the current working directory."""
+
+        def __init__(self, path):
+            self.path = path
+            self._old_cwd = []
+
+        def __enter__(self):
+            self._old_cwd.append(os.getcwd())
+            os.chdir(self.path)
+
+        def __exit__(self, *excinfo):
+            os.chdir(self._old_cwd.pop())
+
+
+if sys.version_info >= (3, 11):
+    pass
+else:
+    from enum import Enum
+
+    # vendored from Python 3.11.0
+    class ReprEnum(Enum):
+        """
+        Only changes the repr(), leaving str() and format() to the mixed-in type.
+        """
+
+    class StrEnum(str, ReprEnum):
+        """
+        Enum where members are also (and must be) strings
+        """
+
+        def __new__(cls, *values):
+            "values must already be of type `str`"
+            if len(values) > 3:
+                raise TypeError(f"too many arguments for str(): {values!r}")
+            if len(values) == 1:
+                # it must be a string
+                if not isinstance(values[0], str):
+                    raise TypeError(f"{values[0]!r} is not a string")
+            if len(values) >= 2:
+                # check that encoding argument is a string
+                if not isinstance(values[1], str):
+                    raise TypeError(f"encoding must be a string, not {values[1]!r}")
+            if len(values) == 3:
+                # check that errors argument is a string
+                if not isinstance(values[2], str):
+                    raise TypeError("errors must be a string, not %r" % (values[2]))
+            value = str(*values)
+            member = str.__new__(cls, value)
+            member._value_ = value
+            return member
+
+        def _generate_next_value_(name, start, count, last_values):
+            """
+            Return the lower-cased version of the member name.
+            """
+            return name.lower()
--- idefix_cli/lib.py
+++ idefix_cli/lib.py
@@ -19,6 +19,11 @@ from typing import Union
 from packaging.version import Version
 from rich.console import Console
 
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from idefix_cli._backports import StrEnum
+
 # workaround mypy not being confortable around decorator preserving signatures
 # adapted from
 # https://github.com/python/mypy/issues/1551#issuecomment-253978622
@@ -46,49 +51,6 @@ __all__ = [
 
 # done as a separate statement to avoid adding a noqa to the whole __all__ def
 __all__.append("chdir")
-
-if sys.version_info >= (3, 11):
-    from enum import StrEnum
-else:
-    from enum import Enum as _Enum
-
-    # vendored from Python 3.11.0
-    class _ReprEnum(_Enum):
-        """
-        Only changes the repr(), leaving str() and format() to the mixed-in type.
-        """
-
-    class StrEnum(str, _ReprEnum):
-        """
-        Enum where members are also (and must be) strings
-        """
-
-        def __new__(cls, *values):
-            "values must already be of type `str`"
-            if len(values) > 3:
-                raise TypeError(f"too many arguments for str(): {values!r}")
-            if len(values) == 1:
-                # it must be a string
-                if not isinstance(values[0], str):
-                    raise TypeError(f"{values[0]!r} is not a string")
-            if len(values) >= 2:
-                # check that encoding argument is a string
-                if not isinstance(values[1], str):
-                    raise TypeError(f"encoding must be a string, not {values[1]!r}")
-            if len(values) == 3:
-                # check that errors argument is a string
-                if not isinstance(values[2], str):
-                    raise TypeError("errors must be a string, not %r" % (values[2]))
-            value = str(*values)
-            member = str.__new__(cls, value)
-            member._value_ = value
-            return member
-
-        def _generate_next_value_(name, start, count, last_values):
-            """
-            Return the lower-cased version of the member name.
-            """
-            return name.lower()
 
 
 if platform.system().lower().startswith("win"):
@@ -397,28 +359,9 @@ def __getattr__(attr: str):
     if attr == "chdir":
         if sys.version_info >= (3, 11):
             from contextlib import chdir
-
-            return chdir
         else:
-            # vendored from Python 3.11b1
-            from contextlib import AbstractContextManager
-
-            class chdir(AbstractContextManager):
-                """Non thread-safe context manager to change the current working directory."""
-
-                def __init__(self, path):
-                    self.path = path
-                    self._old_cwd = []
-
-                def __enter__(self):
-                    self._old_cwd.append(os.getcwd())
-                    os.chdir(self.path)
-
-                def __exit__(self, *excinfo):
-                    os.chdir(self._old_cwd.pop())
-
-            return chdir
-
+            from idefix_cli._backports import chdir
+        return chdir
     else:
         raise AttributeError(f"Unknown attribute {attr!r}")
 
