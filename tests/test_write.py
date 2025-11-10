2d83f5ff2e5b077fb56a657f10c2d528e12a4684
Clément Robert <cr52@protonmail.com>
2025-11-10 11:00:31 +0100

    TST/DEP: replace pytest-check with pytest native subtests

--- tests/test_write.py
+++ tests/test_write.py
@@ -4,7 +4,6 @@ import re
 import subprocess
 
 import pytest
-from pytest_check import check
 
 from idefix_cli.__main__ import idfx_entry_point as main
 
@@ -23,30 +22,30 @@ def normalise_whitespace(s):
     return "\n".join(ret)
 
 
-def test_write_simple_conf(inifile, monkeypatch, tmp_path, capsys):
+def test_write_simple_conf(inifile, capsys, monkeypatch, subtests, tmp_path):
     monkeypatch.setattr("sys.stdin", io.StringIO(simple_conf))
     ret = main(["read", str(inifile.absolute())])
-    with check:
+    with subtests.test():
         assert ret == 0
     out, err = capsys.readouterr()
 
     target = tmp_path / "idefix.mod.ini"
     ret = main(["write", str(target.absolute())])
-    with check:
+    with subtests.test():
         assert ret == 0
 
     out, err = capsys.readouterr()
-    with check:
+    with subtests.test():
         assert out == ""
-    with check:
+    with subtests.test():
         assert err == ""
-    with check:
+    with subtests.test():
         assert target.is_file()
-    with check:
+    with subtests.test():
         assert normalise_whitespace(target.read_text()) == simple_conf_as_ini
 
 
-def test_write_file_exists(tmp_path, capsys, monkeypatch):
+def test_write_file_exists(capsys, monkeypatch, subtests, tmp_path):
     monkeypatch.setattr("sys.stdin", io.StringIO(simple_conf))
 
     target = tmp_path / "idefix.mod.ini"
@@ -54,79 +53,79 @@ def test_write_file_exists(tmp_path, capsys, monkeypatch):
     # a minimalist configuration respecting Idefix inifile format
 
     ret = main(["write", str(target.absolute())])
-    with check:
+    with subtests.test():
         assert ret != 0
     out, err = capsys.readouterr()
-    with check:
+    with subtests.test():
         assert out == ""
-    with check:
+    with subtests.test():
         assert (
             err
             == f"💥 destination file {target} already exists. Use -f/--force to overwrite.\n"
         )
-    with check:
+    with subtests.test():
         assert target.read_text() == ""
 
 
 @pytest.mark.parametrize("flag", ["-f", "--force"])
-def test_write_file_exists_force(flag, tmp_path, capsys, monkeypatch):
+def test_write_file_exists_force(flag, capsys, monkeypatch, subtests, tmp_path):
     monkeypatch.setattr("sys.stdin", io.StringIO(simple_conf))
 
     target = tmp_path / "idefix.mod.ini"
     target.touch()
 
     ret = main(["write", str(target.absolute()), flag])
-    with check:
+    with subtests.test():
         assert ret == 0
 
     out, err = capsys.readouterr()
-    with check:
+    with subtests.test():
         assert out == ""
-    with check:
+    with subtests.test():
         assert err == ""
-    with check:
+    with subtests.test():
         assert target.is_file()
-    with check:
+    with subtests.test():
         assert normalise_whitespace(target.read_text()) == simple_conf_as_ini
 
 
-def test_invalid_json(capsys, tmp_path, monkeypatch):
+def test_invalid_json(capsys, monkeypatch, subtests, tmp_path):
     monkeypatch.setattr("sys.stdin", io.StringIO("1" + simple_conf))
 
     target = tmp_path / "idefix.mod.ini"
 
     ret = main(["write", str(target.absolute())])
-    with check:
+    with subtests.test():
         assert ret != 0
     out, err = capsys.readouterr()
-    with check:
+    with subtests.test():
         assert out == ""
-    with check:
+    with subtests.test():
         assert err == "💥 input is not valid json.\n"
 
     # test that this is still what happens even if the target file exists
     target.touch()
     ret = main(["write", str(target.absolute())])
-    with check:
+    with subtests.test():
         assert ret != 0
     out, err = capsys.readouterr()
-    with check:
+    with subtests.test():
         assert out == ""
-    with check:
+    with subtests.test():
         assert err == "💥 input is not valid json.\n"
 
 
-def test_invalid_inifile(capsys, tmp_path, monkeypatch):
+def test_invalid_inifile(capsys, monkeypatch, subtests, tmp_path):
     invalid_json = json.dumps({"section": {"subsection": {"subsubsection": 2}}})
     monkeypatch.setattr("sys.stdin", io.StringIO(invalid_json))
 
     target = tmp_path / "idefix.mod.ini"
 
     ret = main(["write", str(target.absolute())])
-    with check:
+    with subtests.test():
         assert ret != 0
     out, err = capsys.readouterr()
-    with check:
+    with subtests.test():
         assert out == ""
-    with check:
+    with subtests.test():
         assert err == "💥 input is not Pluto inifile format compliant.\n"
